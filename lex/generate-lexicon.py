95186f0ffca8d826a6d2bae28271d6ac946ff7a5
Mathieu Constant <mathieu.constant@atilf.fr>
2018-04-24 00:25:33 +0200

    make script a module

--- lex/generate-lexicon.py
+++ lex/generate-lexicon.py
@@ -9,32 +9,36 @@ from lex.generatexml import entry2xmlelement
 
 import argparse
 import os
-parser = argparse.ArgumentParser()
-parser.add_argument("table", help="the table filepath to be transformed and outputed")
-parser.add_argument("out", help="output dir")
-args = parser.parse_args()
 
-name = os.path.basename(args.table)
-#print(name[:-8])
-doc = Document()
-root = doc.createElement('lex')
-doc.appendChild(root)
+if __name__ == "__main__":
 
-f = open(args.table)
-fout = open(args.out+'/'+name[:-8]+'.xml','w')
-titles=[]
-i=0
-for l in f:
-   tab = [s[1:-1] for s in l[:-1].split(';')]
-   if len(tab) == 1:
-       break
-   #print(tab)
-   #print(titles)
-   if i == 0: #this is the title line
-       titles = tab
-       properties = []
-       properties2 = [] # properties to apply once all others are applied
-       for prop in titles:
+   parser = argparse.ArgumentParser()
+   parser.add_argument("table", help="the table filepath to be transformed and outputed")
+   parser.add_argument("out", help="output dir")
+   args = parser.parse_args()
+
+   name = os.path.basename(args.table)
+   #print(name[:-8])
+   doc = Document()
+   root = doc.createElement('lex')
+   doc.appendChild(root)
+
+   f = open(args.table)
+   fout = open(args.out+'/'+name[:-8]+'.xml','w')
+   fout2 = open(args.out+'/'+name[:-8]+'.txt','w')
+   titles=[]
+   i=0
+   for l in f:
+      tab = [s[1:-1] for s in l[:-1].split(';')]
+      if len(tab) == 1:
+         break
+      #print(tab)
+      #print(titles)
+      if i == 0: #this is the title line
+         titles = tab
+         properties = []
+         properties2 = [] # properties to apply once all others are applied
+         for prop in titles:
            if FreeConstituentDistribution.isOfType(prop):
                properties.append(FreeConstituentDistribution(prop))
                properties2.append(None)
@@ -62,26 +66,20 @@ for l in f:
            else:
                 properties.append(None)
                 properties2.append(None)
-   else:
-       e = entry(name[:-8])
-       e.load_entry(properties, properties2, tab)
-       post_processing(e)
-       #print(e)
+      else:
+         e = entry(name[:-8])
+         e.load_entry(properties, properties2, tab)
+         post_processing(e)
+         fout2.write(str(e))
 
-       root.appendChild(entry2xmlelement(e, doc))
+         root.appendChild(entry2xmlelement(e, doc))
 
 
+      i+=1
 
-       #for j in range(len(properties)):
-       #    if properties[j] is not None:
-       #        print(str(properties[j]))
-       #        print(properties[j].apply(None,tab[j]))
-   #print(titles)
-   #print(tab)
-   i+=1
-
-doc.writexml(fout, indent="   ", addindent="   ", newl='\n')
-f.close()
-fout.close()
+   doc.writexml(fout, indent="   ", addindent="   ", newl='\n')
+   f.close()
+   fout.close()
+   fout2.close()
 
 
